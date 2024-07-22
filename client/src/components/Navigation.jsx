446caa870f473a4115ec0535ed8f756ae1878d27
Amit saini <amitsaini2001.18@gmail.com>
2024-07-22 12:56:58 +0530

    heading

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <nav className="flex px-8 items-center w-full h-[75px] shadow-md border-b-[1px]">
+        <img
+          className="w-[130px]"
+          src="https://deftsoft.com/assets/images/deft-logo2.svg"
+          alt=""
+        />
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
--- client/src/view/TL.jsx
+++ client/src/view/TL.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navigation from "../components/Navigation";
 
 const TL = () => {
-  return <div className="w-full h-screen bg-slate-400">TL</div>;
+  return (
+    <div className="w-full h-screen">
+      <Navigation />
+    </div>
+  );
 };
 
 export default TL;
