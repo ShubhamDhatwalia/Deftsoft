f72d0893f6f30b4e023afae09bc76d48d1bb9db3
Amit saini <amitsaini2001.18@gmail.com>
2024-07-24 13:05:01 +0530

    edition

--- client/src/view/TL.jsx
+++ client/src/view/TL.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import Navigation from "../components/Navigation";
 
 const TL = () => {
-
-  return <div>Nikhil </div>;
   return (
     <div className="w-full min-h-screen">
       <Navigation />
     </div>
   );
-
 };
 
 export default TL;
