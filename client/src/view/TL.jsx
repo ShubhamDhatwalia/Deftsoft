47904b9f01df08412cfdf873e5f1d2e891bce17c
Nikhil Harchand <nikhilharchand@gmail.com>
2024-07-24 10:57:28 +0530

    testing

--- client/src/view/TL.jsx
+++ client/src/view/TL.jsx
@@ -3,7 +3,7 @@ import Navigation from "../components/Navigation";
 
 const TL = () => {
 
-  return <div>Nikhil testing</div>;
+  return <div>Nikhil </div>;
   return (
     <div className="w-full h-screen">
       <Navigation />
