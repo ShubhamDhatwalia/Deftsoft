fc1202bc369d30e7288f1df81d8cd146e9926df7
Sanchit <jarvus972001@gmail.com>
2024-07-26 09:56:42 +0530

    sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import TL from "./view/TL";
 import Admin from "./view/Admin";
 import PM from "./view/PM";
 import Employee from "./view/Employee";
-import  EmpHome  from "./emppages/EmpHome";
+import EmpHome from "./emppages/EmpHome";
 import { Task } from "./emppages/Task";
 import { Leaves } from "./emppages/Leaves";
 import { Todo } from "./emppages/Todo";
@@ -19,13 +19,14 @@ const App = () => {
         <Route path="/tl" element={<TL />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/pm" element={<PM />} />
-        <Route path="/employee" element={<Employee />} >
-          <Route path="emphome" element={<EmpHome/>} />
+        <Route path="/employee" element={<Employee />}>
+          <Route index element={<EmpHome />} />
+          <Route path="emphome" element={<EmpHome />} />
           <Route path="task" element={<Task />} />
           <Route path="leaves" element={<Leaves />} />
           <Route path="todo" element={<Todo />} />
           <Route path="calendar" element={<Calendar />} />
-         </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
