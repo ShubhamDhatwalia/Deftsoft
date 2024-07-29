6679d65f0034c274c51ae16683d75df9baa97ba4
Dhatwalia <dhatwaliashubham3@gmail.com>
2024-07-29 13:06:28 +0530

    practice

--- client/src/view/Admin.jsx
+++ client/src/view/Admin.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return
+  <>
+    <div>Admin</div>;
+    <div>Admin</div>;
+    
+  </>;
+  
 };
 
 export default Admin;
