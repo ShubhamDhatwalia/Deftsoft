344e4bdd3f7fd6b30e398a9f75bc955607b815f0
Shubham Dhatwalia <shubhamdhatwalia3@gmail.com>
2024-08-04 16:58:17 +0530

    update

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { LuLogOut } from "react-icons/lu";
 import SideDrawer from "./SideDrawer";
 import { Link } from "react-router-dom";
+import { LuMenu } from "react-icons/lu";
 
 const Navigation = () => {
   return (
     <>
       <div className="h-[58px]">
         <nav className="flex fixed bg-white top-0 px-8 py-1 items-center w-full  shadow-md border-b-[1px] justify-between z-50 ">
+          
+          
+        <LuMenu size={25} className="hidden nav-toggle"/>
+          
           <Link to="">
             <img
               className="w-[130px] nav-logo"
@@ -19,24 +24,14 @@ const Navigation = () => {
             <SideDrawer />
             <LuLogOut
               size={40}
-              className=" rounded-full p-1 hover:bg-rose-300 transition duration-300 cursor-pointer"
+              className=" logout-icon rounded-full p-1 hover:bg-rose-300 transition duration-300 cursor-pointer"
             />
           </div>
         </nav>
       </div>
     </>
 
-    // <nav className="fixed top-0 left-0 right-0 flex px-8 items-center w-full h-[60px] shadow-md border-b-[1px] justify-between bg-white z-50">
-    //   <img
-    //     className="w-[130px]"
-    //     src="https://deftsoft.com/assets/images/deft-logo2.svg"
-    //     alt=""
-    //   />
-    //   <div className="flex gap-8">
-    //     <SideDrawer />
-    //     <LuLogOut size={35} />
-    //   </div>
-    // </nav>
+  
   );
 };
 
