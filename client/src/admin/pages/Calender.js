f766335102d374fbea52e126cdf9c4d3671d43a8
Shubham Dhatwalia <shubhamdhatwalia3@gmail.com>
2024-08-08 21:42:02 +0530

    remaining only event time functionality from eventModel also

--- client/src/admin/pages/Calender.js
+++ client/src/admin/pages/Calender.js
@@ -1,41 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import useCalender from "../../store/calendar";
 import { createEventId } from "../../store/Data";
-import EventModal from "../EventModel"; 
+import EventModal from "../EventModal";
+import EventDetailsModal from "../EventDetailsModal";
 
 function Calender() {
   const { currentEvents, setCurrentEvents } = useCalender();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false);
   const [selectInfo, setSelectInfo] = useState(null);
+  const [selectedEvent, setSelectedEvent] = useState(null);
 
-  const handleEvents = async (events) => {
-    await Promise.resolve(setCurrentEvents(events));
+  const calendarRef = useRef(null);
+
+  const handleEvents = (events) => {
+    setCurrentEvents(events);
   };
 
   const handleDateSelect = (selectInfo) => {
     setSelectInfo(selectInfo);
     setIsModalOpen(true);
   };
-
-  const handleSaveEvent = (title) => {
+  const handleSaveEvent = (title, description) => {
     if (selectInfo) {
-      let calendarApi = selectInfo.view.calendar;
-
+      let calendarApi = calendarRef.current.getApi();
+  
       calendarApi.unselect();
-
+  
       if (title) {
-        calendarApi.addEvent({
+        const newEvent = {
           id: createEventId(),
           title,
-          start: selectInfo.start,
-          end: selectInfo.end,
+          start: selectInfo.startStr,
+          end: selectInfo.endStr,
           allDay: selectInfo.allDay,
+          extendedProps: {
+            description,
+          },
+        };
+  
+        calendarApi.addEvent(newEvent);
+        setCurrentEvents((prevEvents) => {
+          if (Array.isArray(prevEvents)) {
+            return [...prevEvents, newEvent];
+          }
+          return [newEvent];
         });
       }
+    }
+  };
+  
+
+  const handleEventClick = (clickInfo) => {
+    setSelectedEvent(clickInfo.event);
+    setIsDetailsModalOpen(true);
+  };
+
+  const handleDeleteEvent = (eventId) => {
+    if (calendarRef.current) {
+      let calendarApi = calendarRef.current.getApi();
+
+      setCurrentEvents((prevEvents) => {
+        if (Array.isArray(prevEvents)) {
+          const filteredEvents = prevEvents.filter((event) => event.id !== eventId);
+          return filteredEvents;
+        }
+        return [];
+      });
+
+      const event = calendarApi.getEventById(eventId);
+      if (event) {
+        event.remove();
+      }
+
+      setIsDetailsModalOpen(false);
     }
   };
 
@@ -44,6 +86,7 @@ function Calender() {
       <div className="calendar-container mx-5 p-4 h-[90vh] w-[70vw]">
         <div className="fullcalendar-wrapper h-full">
           <FullCalendar
+            ref={calendarRef}
             plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
             headerToolbar={{
               left: "prev,next today",
@@ -62,6 +105,7 @@ function Calender() {
             initialEvents={currentEvents}
             eventsSet={handleEvents}
             select={handleDateSelect}
+            eventClick={handleEventClick}
             height="100%"
             contentHeight="100%"
           />
@@ -71,6 +115,12 @@ function Calender() {
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
         onSave={handleSaveEvent}
+      />
+      <EventDetailsModal
+        isOpen={isDetailsModalOpen}
+        onClose={() => setIsDetailsModalOpen(false)}
+        event={selectedEvent}
+        onDelete={handleDeleteEvent}
       />
     </>
   );
