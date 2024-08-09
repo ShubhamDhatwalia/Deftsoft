32f3181840510beef73ddea57c99731b423c5432
Dhatwalia <dhatwaliashubham3@gmail.com>
2024-08-09 13:25:00 +0530

    calender updates

--- client/src/admin/EventModal.jsx
+++ client/src/admin/EventModal.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 
 function EventModal({ isOpen, onClose, onSave }) {
   const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("");
   const [description, setDescription] = useState("");
 
   const handleSave = () => {
     if (title) {
-      onSave(title, description);
+      onSave(title, description, location);
       setTitle("");
       setDescription("");
+      setLocation("");
       onClose();
     }
   };
@@ -19,33 +21,44 @@ function EventModal({ isOpen, onClose, onSave }) {
     <div className="fixed inset-0 bg-black bg-opacity-45 flex items-center justify-center z-10">
       <div className="bg-white p-4 rounded-lg flex flex-col">
         <h2 className="text-xl font-semibold mb-4">Add Event</h2>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Enter event title"
-          className="border-2 border-slate-200 bg-slate-200 hover:border-blue-200 cursor-pointer focus:outline-blue-200 mb-4 rounded-xl px-4 text-lg pr-10 h-10"
-        />
-        <textarea
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="Enter event description"
-          className="border-2 border-slate-200 bg-slate-200 hover:border-blue-200 cursor-pointer focus:outline-blue-200 mb-4 rounded-xl px-4 text-lg pr-10 h-20"
-        />
-        <div className="flex justify-between">
-          <button
-            onClick={onClose}
-            className="bg-gray-500 text-lg font-medium text-white px-4 py-1 rounded-lg hover:bg-gray-600"
-          >
-            Cancel
-          </button>
-          <button
-            onClick={handleSave}
-            className="bg-blue-500 text-lg font-medium text-white px-4 py-1 rounded-lg hover:bg-blue-600"
-          >
-            Save
-          </button>
-        </div>
+        <form action="" className="flex flex-col">
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Enter event title"
+            className="border-2 border-blue-100 bg-blue-100 hover:border-blue-400 cursor-pointer focus:outline-blue-400 mb-2 rounded-xl px-4 text-lg pr-10 h-10"
+          />
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Enter event description"
+            className="border-2 border-blue-100 bg-blue-100 hover:border-blue-400 cursor-pointer focus:outline-blue-400 mb-2 rounded-xl px-4 text-lg pr-10 h-14"
+          />
+
+          <input
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="location"
+            className="border-2 border-blue-100 bg-blue-100 hover:border-blue-400 cursor-pointer focus:outline-blue-400 mb-2 rounded-xl px-4 text-lg pr-10 h-10"
+          />
+          <div className="flex justify-between">
+            <button
+              onClick={onClose}
+              className="bg-gray-500 text-lg font-medium text-white px-4 py-1 rounded-lg hover:bg-gray-600"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={handleSave}
+              className="bg-blue-500 text-lg font-medium text-white px-4 py-1 rounded-lg hover:bg-blue-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- client/src/admin/pages/Calender.js
+++ client/src/admin/pages/Calender.js
@@ -3,34 +3,31 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
 import useCalender from "../../store/calendar";
 import { createEventId } from "../../store/Data";
 import EventModal from "../EventModal";
-import EventDetailsModal from "../EventDetailsModal";
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css"; // Import Tippy's default styles
 
 function Calender() {
   const { currentEvents, setCurrentEvents } = useCalender();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false);
   const [selectInfo, setSelectInfo] = useState(null);
-  const [selectedEvent, setSelectedEvent] = useState(null);
 
   const calendarRef = useRef(null);
-
-  const handleEvents = (events) => {
-    setCurrentEvents(events);
-  };
 
   const handleDateSelect = (selectInfo) => {
     setSelectInfo(selectInfo);
     setIsModalOpen(true);
   };
+
   const handleSaveEvent = (title, description) => {
     if (selectInfo) {
       let calendarApi = calendarRef.current.getApi();
-  
+
       calendarApi.unselect();
-  
+
       if (title) {
         const newEvent = {
           id: createEventId(),
@@ -40,44 +37,57 @@ function Calender() {
           allDay: selectInfo.allDay,
           extendedProps: {
             description,
+            location: "Sample Location", // You can change or add location dynamically
           },
         };
-  
+
         calendarApi.addEvent(newEvent);
-        setCurrentEvents((prevEvents) => {
-          if (Array.isArray(prevEvents)) {
-            return [...prevEvents, newEvent];
-          }
-          return [newEvent];
-        });
+        setCurrentEvents((prevEvents) => [...prevEvents, newEvent]);
       }
     }
   };
-  
 
-  const handleEventClick = (clickInfo) => {
-    setSelectedEvent(clickInfo.event);
-    setIsDetailsModalOpen(true);
+  const handleEventDidMount = (info) => {
+    const { event } = info;
+    const { title, start, end, extendedProps } = event;
+
+    const content = `
+      <div class="tooltip-content max-w-xs ">
+        <div class="tooltip-title text-center font-bold text-lg mb-1 break-words">${title}</div>
+        <div class="tooltip-description text-sm  break-words">
+          ${extendedProps.description || "No description"}
+        </div>
+        <div class="tooltip-location text-sm font-semibold  break-words">
+          ${extendedProps.location || "No location"}
+        </div>
+        <div class="tooltip-time text-sm mb-2">
+          <strong>Start:</strong> ${start.toLocaleString()}<br>
+          <strong>End:</strong> ${end ? end.toLocaleString() : "No end time"}
+        </div>
+      </div>
+    `;
+
+    tippy(info.el, {
+      content: content,
+      theme: "custom",
+      placement: "top",
+      trigger: "mouseenter",
+      arrow: true,
+      allowHTML: true,
+    });
   };
 
-  const handleDeleteEvent = (eventId) => {
-    if (calendarRef.current) {
-      let calendarApi = calendarRef.current.getApi();
+  const handleEventClick = (clickInfo) => {
+    if (window.confirm("Are you sure you want to delete this event?")) {
+      const eventId = clickInfo.event.id;
 
-      setCurrentEvents((prevEvents) => {
-        if (Array.isArray(prevEvents)) {
-          const filteredEvents = prevEvents.filter((event) => event.id !== eventId);
-          return filteredEvents;
-        }
-        return [];
-      });
+      // Remove event from FullCalendar
+      clickInfo.event.remove();
 
-      const event = calendarApi.getEventById(eventId);
-      if (event) {
-        event.remove();
-      }
-
-      setIsDetailsModalOpen(false);
+      // Update state to remove event from `currentEvents`
+      setCurrentEvents((prevEvents) =>
+        prevEvents.filter((event) => event.id !== eventId)
+      );
     }
   };
 
@@ -87,25 +97,31 @@ function Calender() {
         <div className="fullcalendar-wrapper h-full">
           <FullCalendar
             ref={calendarRef}
-            plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+            plugins={[
+              dayGridPlugin,
+              interactionPlugin,
+              timeGridPlugin,
+              listPlugin,
+            ]}
             headerToolbar={{
               left: "prev,next today",
               center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay",
+              right: "dayGridMonth,timeGridWeek,listMonth",
             }}
             allDaySlot={true}
+            
             initialView="dayGridMonth"
-            slotDuration="01:00:00"
+            slotDuration="00:30:00"
             editable={true}
             selectable={true}
             selectMirror={true}
-            dayMaxEvents={true}
+            dayMaxEvents={1}
             weekends={true}
             nowIndicator={true}
             initialEvents={currentEvents}
-            eventsSet={handleEvents}
             select={handleDateSelect}
-            eventClick={handleEventClick}
+            eventDidMount={handleEventDidMount}
+            eventClick={handleEventClick} // Add eventClick handler
             height="100%"
             contentHeight="100%"
           />
@@ -115,12 +131,6 @@ function Calender() {
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
         onSave={handleSaveEvent}
-      />
-      <EventDetailsModal
-        isOpen={isDetailsModalOpen}
-        onClose={() => setIsDetailsModalOpen(false)}
-        event={selectedEvent}
-        onDelete={handleDeleteEvent}
       />
     </>
   );
